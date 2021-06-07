28444e302de5cc67efa384915d86601b8398626c
Nabil Ridhwan <nabridhwan@gmail.com>
2021-06-07 17:52:58 +0800

    Week 7 final

--- FOP/week7-functions/p7q1.js
+++ FOP/week7-functions/p7q1.js
@@ -5,7 +5,7 @@
 
 function printTable() {
     for(var i = 0; i <= 1000; i++){
-        i % 5 == 0 ? console.log(i) : null
+        console.log(`5 * ${i} = ${i*5}`)
     }
 }
 
---
+++ FOP/week7-functions/t7q1.js
@@ -0,0 +1,13 @@
+function function1() {
+    str1 = "hello";
+    console.log(str1 + " & " + str2);
+}
+
+function function2() {
+    console.log(str2 + ", " + str1);
+}
+
+var str2 = "bye bye"; 
+
+function2()
+function1()
