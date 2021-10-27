588da4a1d3a01c2f618868cc50afec2398ec545b
Nabil Ridhwan <nabridhwan@gmail.com>
2021-10-27 19:34:58 +0800

    Practical 3 BED

---
+++ BED/practical3/index.js
@@ -0,0 +1,29 @@
+let express = require("express")
+let app = express();
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+
+app.delete("/user/:id", (req, res) => {
+
+    let {
+        id
+    } = req.params;
+
+    res.status(200).send({
+        "message": `User with ${id} has been successfully deleted!`
+    })
+})
+
+app.put("/user/:id", (req, res) => {
+
+    let {
+        id
+    } = req.params;
+    let {email} = req.body;
+    res.status(200).send(`User with ${id} has been successfully updated with new email ${email}!`)
+})
+
+app.listen(8081, () => {
+    console.log("Server is running on port 8081");
+})
