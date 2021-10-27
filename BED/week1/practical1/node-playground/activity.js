28e23188dce617ecc7a40203fb5829e68402fad2
Nabil Ridhwan <nabridhwan@gmail.com>
2021-10-27 19:35:17 +0800

    test2 txt

--- BED/week1/practical1/node-playground/activity.js
+++ BED/week1/practical1/node-playground/activity.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
-const fileDir = "./test2.txt"
 
 // This function will "Hello World to the file"
-fs.writeFile(fileDir, "Hello World\n", {
-    flag: "a"
-}, (err) => {
+fs.writeFile("./test2.txt", "Hello World\n", (err) => {
 
     if (err) console.log(err)
 
     // Read file
-    let data = fs.readFileSync(fileDir);
-    console.log(data.toString());
+    fs.readFile("./test2.txt", 'utf8', (err,data) => {
+        console.log(data);
+    });
 });
