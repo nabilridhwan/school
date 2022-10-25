525e7af6c362abc080be5c1b80e8bb03b0328d9b
Nabil Ridhwan <nabridhwan@gmail.com>
2022-10-25 10:33:27 +0800

    Add Practical 4 and 5 for ANDE

--- ANDE/practical-01-installation/app/src/main/java/com/example/helloworld/SplashScreen.java
+++ ANDE/practical-01-installation/app/src/main/java/com/example/helloworld/SplashScreen.java
@@ -1,2 +1,35 @@
-package com.example.helloworld;public class SplashScreen {
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        splashThread.start();
+    }
+
 }
+
