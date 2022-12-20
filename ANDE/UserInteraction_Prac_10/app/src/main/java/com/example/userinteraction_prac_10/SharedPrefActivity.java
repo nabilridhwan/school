310c20249411bd0ef90063a69840977102721428
Nabil Ridhwan <nabridhwan@gmail.com>
2022-12-20 14:01:13 +0800

    latest changes

--- ANDE/UserInteraction_Prac_10/app/src/main/java/com/example/userinteraction_prac_10/SharedPrefActivity.java
+++ ANDE/UserInteraction_Prac_10/app/src/main/java/com/example/userinteraction_prac_10/SharedPrefActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
